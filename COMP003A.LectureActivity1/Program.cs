ff2505344fd557c90b929ec9897e1a29c9c928d5
matthewtan <matthewtan@LEM231-08>
2023-01-18 18:56:12 -0800

    Add project files.

---
+++ COMP003A.LectureActivity1/Program.cs
@@ -0,0 +1,18 @@
+﻿// This is a single line comment
+/* This is a multi-line comment 
+ * Author: Matthew Tan
+ * Course: COMP-003A
+ * Purpose: Lecture activity for this week
+*/
+namespace COMP003A.LectureActivity1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); // add comments here too
+            Console.WriteLine("test");
+            Console.WriteLine("Matthew Tan");
+        }
+    }
+}
